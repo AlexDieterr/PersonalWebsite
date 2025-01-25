c3d369f853e5a6142208f18b7264688edb1e77ea
AlexDieterr <alexdieter1@gmail.com>
2025-01-24 22:30:24 -0800

    add hero and contact placeholder components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HeroComponent }, // Hero as the homepage
+    { path: 'contact', component: ContactComponent } // Contact page
+  ];
