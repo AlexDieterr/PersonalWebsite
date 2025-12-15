376c6b1af4124d1d6ecfba71076764115f41bbe3
AlexDieterr <alexdieter1@gmail.com>
2025-12-15 12:44:35 -0800

    trying to fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,5 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'projects/:id', component: ProjectDetailsComponent },
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: '' }
 ];
