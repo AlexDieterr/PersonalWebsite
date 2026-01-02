67ab41c453c5dfe612b35909b83af8b4afbb0a96
AlexDieterr <alexdieter1@gmail.com>
2026-01-01 17:15:03 -0800

    added email form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
+
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  form = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  loading = false;
+  success = false;
+  error = false;
+
+  sendEmail() {
+    this.loading = true;
+    this.success = false;
+    this.error = false;
+
+    emailjs.send(
+      'service_k4vby0g',
+      'template_bifxwfl',
+      {
+        from_name: this.form.name,
+        from_email: this.form.email,
+        message: this.form.message
+      },
+      'uVSKrQXyx7R_cuHA3'
+    )
+    .then(() => {
+      this.success = true;
+      this.loading = false;
+      this.form = { name: '', email: '', message: '' };
+    })
+    .catch(() => {
+      this.error = true;
+      this.loading = false;
+    });
+  }
 }
