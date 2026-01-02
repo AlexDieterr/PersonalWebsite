973d63dfbc34522da7a8584e72bd3ccb6c04fc36
AlexDieterr <alexdieter1@gmail.com>
2026-01-01 19:48:37 -0800

    added timeline component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,12 @@ import { HeroComponent } from '../hero/hero.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
+import { TimelineComponent } from "../timeline/timeline.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeroComponent, ProjectsComponent, AboutComponent, ContactComponent],
+  imports: [CommonModule, RouterModule, HeroComponent, ProjectsComponent, AboutComponent, ContactComponent, TimelineComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
