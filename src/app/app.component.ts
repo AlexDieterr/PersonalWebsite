9a6b4f08fd11518e4c3b197d30c0c861647f65f9
AlexDieterr <alexdieter1@gmail.com>
2026-01-29 21:09:57 -0800

    added notice for mobile users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent,CommonModule],
 })
 export class AppComponent implements OnInit {
+  showMobileNotice = false;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.pingTrafficApi();
+    this.checkMobileNotice();
   }
+
+  checkMobileNotice(): void {
+    const isMobile = window.innerWidth <= 768;
+    const dismissed = localStorage.getItem('hideMobileNotice');
+
+    if (isMobile && !dismissed) {
+      this.showMobileNotice = true;
+    }
+  }
+
+  dismissMobileNotice(): void {
+    localStorage.setItem('hideMobileNotice', 'true');
+    this.showMobileNotice = false;
+  }
+
   pingTrafficApi(): void {
     this.http.get('https://trafficsignnn.onrender.com/health').subscribe({
       next: () => {
-        // Warm predict endpoint (no UI impact)
         const formData = new FormData();
         formData.append(
           'file',
@@ -34,6 +52,7 @@ export class AppComponent implements OnInit {
       error: () => {},
     });
   }
+
   scrollToSection(sectionId: string) {
     const section = document.getElementById(sectionId);
     if (section) {
